9fc358a2061062dd3a5630500f23d4c8fe3b5bb8
codepen24 <codepen24@gmail.com>
2019-12-01 23:20:23 +0800

    -

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 ﻿
 
- <p><a href="http://facebook.com/profile.php?=73322363" target="_blank"> Click this link to find out who?</a>This person who is cheating on you is no other than ...........</p>
+ <p><a href="http://facebook.com/profile.php?=73322363" target="_blank"> Click this link to find out who?</a> This person who is cheating on you is no other than ...........</p>
 
