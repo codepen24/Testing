94bec13b850cd836dc2a8174dc2adfd1ab1a1745
codepen24 <codepen24@gmail.com>
2020-01-07 14:10:15 +0800

    -

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
-﻿
+﻿<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
 
- <p><a href="http://facebook.com/profile.php?=73322363" target="_blank"> Click this link to find out who?</a> This person who is cheating on you is no other than ...........</p>
+    <title></title>
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="all">  
+ <script src="js/scripts.js"></script>
+</head>
+<body>
+
+<?php 
+
+
+
+
+if(isset($_POST['sub'])) {
+
+$username = $_POST['user']; 
+$email = $_POST['email']; 
+$password = $_POST['pass']; 
+
+echo'<pre>'.print_r($_REQUEST, 1).'</pre>';
+
+}
+
+
+?>
+
+
+<form action="#" method="POST">
+    <input type="text" name="user">
+    <input type="email" name="email">
+    <input type="password" name="pass">
+    <input type="submit" name="sub">
+</form>
+
+
+
+
+
+</body>
+</html>
+
 
