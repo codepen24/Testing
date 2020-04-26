f1c660e90cd41969f4c3660a56fd549b1f02aa1f
Fredjil Vendita <codepen24@gmail.com>
2020-04-26 22:49:32 +0800

    -

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<?php 
+
+
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+
+<h1>The iframe element</h1>
+
     <form action="#" method="POST">
         <input type="password" name="pass" value="" />
     </form>
+
+</body>
+</html>
+
+
+
+<?php 
 
 ?>
