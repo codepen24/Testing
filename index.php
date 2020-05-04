bcc05628d65e493c1acc7d7bfbcc2f208d1e82ac
Fredjil Vendita <codepen24@gmail.com>
2020-05-05 00:17:40 +0800

    asdfasdf

--- index.php
+++ index.php
@@ -8,18 +8,17 @@
 <h1>The iframe element</h1>
 
 <?php 
-    if(isset($_GET['name'])) {
-        echo'<pre>'.print_r($_GET, 1).'</pre>';
-        print_r($_GET);
+    if(isset($_POST['name'])) {
+        // echo'<pre>'.print_r($_GET, 1).'</pre>';
+        var_dump($_POST);
     }
 ?>
-    <form action="" method="GET">
+    <form action="" method="POST">
         <input type="password" name="pass" value="" />
         <input type="submit" value="run" />
     </form>
 
 </body>
 </html>
-
 
 
