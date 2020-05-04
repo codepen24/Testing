d1d4902bc5796b6a5bd4179a5e68b1edf4a2bd5d
Fredjil Vendita <codepen24@gmail.com>
2020-05-05 00:20:26 +0800

    asdfasdf

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <h1>The iframe element</h1>
 
 <?php 
-    if(isset($_POST['name'])) {
+    if(isset($_POST['run'])) {
         // echo'<pre>'.print_r($_GET, 1).'</pre>';
         var_dump($_POST);
     }
