503dfb01de03c796fb4f403badae1732a9eb4435
= <codepen24@gmail.com>
2021-05-02 22:25:08 +0800

    local

--- functions.php
+++ functions.php
@@ -4,14 +4,4 @@ function seniman_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-
-
-add_action('wp_head', 'get_what_page' );
-function get_what_page() {
-    global $post;
-    $id = $post->id;
-    function get_what_page_pres($g_var) {
-        echo '<pre>' . var_dump($g_var) . '</pre>'; 
-    }
-    return get_what_page_pres(get_page($id)); 
-}
+?>
